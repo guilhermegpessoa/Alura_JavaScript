be01a03ec53752d8a8cef2827d64905e9920209a
Guilherme G. Pessoa <gpessoa@ciandt.com>
2021-05-11 14:34:59 -0300

    Validação de Formulários

--- js/form.js
+++ js/form.js
@@ -7,14 +7,33 @@ botaoAdicionar.addEventListener("click", function(event){
 
   var pacienteTr = montaTr(paciente);
   
-  // adicionando paciente na tabela
+  var erros = validaPaciente(paciente);
+
+  if(erros.length > 0){
+    exibeMensagensDeErro(erros);
+    return;
+  }
+
   var tabela = document.querySelector("#tabela-pacientes");
 
   tabela.appendChild(pacienteTr);
 
   form.reset();
-  
+  var mensagensErro = document.querySelector("#mensagens-erro");
+  mensagensErro.innerHTML = "";
+
 });
+
+function exibeMensagensDeErro(erros){
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = ""; 
+
+  erros.forEach(function(erro){
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
 
 function obtemPacienteDoFormulario(form){
 
@@ -48,4 +67,35 @@ function montaTd(dado, classe){
   td.classList.add(classe);
 
   return td;
+}
+
+function validaPaciente(paciente){
+
+  var erros = [];
+
+  if(paciente.nome.length == 0){
+    erros.push("Nome não pode ser em branco!")
+  }
+
+  if(!validaPeso(paciente.peso)){
+    erros.push("Peso inválido!");
+  }
+
+  if(!validaAltura(paciente.altura)){
+    erros.push("Altura inválida!");
+  }
+
+  if(paciente.gordura.length == 0){
+    erros.push("Gordura não pode ser em branco!");
+  }
+
+  if(paciente.peso.length == 0){
+    erros.push("Peso não pode ser em branco!")
+  }
+
+  if(paciente.altura.length == 0){
+    erros.push("Altura não pode ser em branco!")
+  }
+
+  return erros;
 }
